0ed3cdb6fd7b19dbc85ddecd40d0e85560ad0c69
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-07-28 15:11:32 +0200

    Wrote code the make test sui pass

---
+++ src/api.js
@@ -0,0 +1,101 @@
+/* eslint-disable no-useless-concat */
+import mockData from "./mock-data";
+
+/**
+ *
+ * @param {*} events:
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ */
+export const extractLocations = (events) => {
+  const extractedLocations = events.map((event) => event.location);
+  const locations = [...new Set(extractedLocations)];
+  return locations;
+}; 
+
+export const getAccessToken = async () => {             
+  const accessToken = localStorage.getItem("access_token");
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        ""
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
+
+
+const checkToken = async (accessToken) => {
+  const response = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  );
+  const result = await response.json();
+  return result;
+};
+ 
+const getToken = async (code) => {
+  try {
+    const encodeCode = encodeURIComponent(code);
+ 
+    // eslint-disable-next-line no-useless-concat
+    const response = await fetch();
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+    return access_token;
+  } catch (error) {
+    return error;
+  }
+ }
+
+export const getEvents = async () => {
+  if (window.location.href.startsWith("http://localhost")) {
+    return mockData;
+  }
+
+  const token = await getAccessToken();
+
+  const removeQuery = () => {
+    let newurl;
+    if (window.history.pushState && window.location.pathname) {
+      newurl =
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        window.location.pathname;
+      window.history.pushState("", "", newurl);
+    } else {
+      newurl = window.location.protocol + "//" + window.location.host;
+      window.history.pushState("", "", newurl);
+    }
+  };
+
+  if (token) {
+    removeQuery();
+    // eslint-disable-next-line no-useless-concat
+    const url = "https://ms8edqfzoi.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
+    const response = await fetch(url)
+    const result = await response.json();
+    if (result) {
+      return result.events;
+    } else {
+      return console.log("No events found"); 
+    }
+  }
+}; 
+
+
