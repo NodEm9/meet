3f768777e91ed6d56679654fbefbf67787102a23
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-08-01 14:39:45 +0200

    Refactor  getEvents function

--- src/components/Event.js
+++ src/components/Event.js
@@ -4,26 +4,28 @@ import PropTypes from 'prop-types'
 
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false)
+  // const formattedDate = new Intl.DateTimeFormat('en-UK', {  dateStyle: 'full', timeStyle: 'long' }).format(new Date(event.start.dateTime))
   return (
     <li className='event'>
       <span className='eventSummary'>
-        <h2>{event.summary}</h2>  
-      <p>{event.location}</p> 
-      <p>{event.created}</p>
-     </span>
-      <button
-        className='showDetailsButton' 
-        onClick={() => setShowDetails(!showDetails)}
-      >
-        {showDetails ? 'Hide Details' : 'Show Details'}
-      </button>
+        <h2>{event.summary}</h2>
+        <p className="location">{event.location}</p>
+        <p className="created">{event && (new Date(event.created)).toUTCString()}</p>
+      </span>
       {showDetails ? (
-        <div className='details'>
-          <h3>Event Details:</h3>
+        <div className='details' id="details" >
           <p>{event.description}</p>
           <a href={event.htmlLink}>See details on Google Calendar</a>
         </div>
       ) : null}
+      <button 
+        id="show-details-btn"
+        className='details-btn'
+        name="Show Details" 
+        onClick={() => setShowDetails(!showDetails)}
+      >
+        {showDetails ? 'Hide Details' : 'Show Details'}
+      </button>
     </li>
   )
 }
