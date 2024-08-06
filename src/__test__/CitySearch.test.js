75dc3b647341c94af25acb5b0bcb2bc7fe17b5d9
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-08-06 10:20:47 +0200

    Pass the infoAleart props to CitySearch render

--- src/__test__/CitySearch.test.js
+++ src/__test__/CitySearch.test.js
@@ -11,7 +11,10 @@ import App from "../App";
 describe('<CitySearch /> component', () => { 
   let CitySearchComponent;
   beforeEach(() => {
-    CitySearchComponent = render(<CitySearch allLocations={[]} />);
+    CitySearchComponent = render(<CitySearch
+      allLocations={[]}
+ 
+    />);
   });
 
   test('renders text input', () => {
@@ -38,7 +41,10 @@ describe('<CitySearch /> component', () => {
     const user = userEvent.setup();
     const allEvents = await getEvents(); 
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    CitySearchComponent.rerender(<CitySearch
+      allLocations={allLocations} 
+      setInfoAlert={() => { }}
+    />);
 
     // user types "Berlin" in city textbox
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -64,6 +70,7 @@ describe('<CitySearch /> component', () => {
     CitySearchComponent.rerender(<CitySearch
       allLocations={allLocations}
       setCurrentCity={() => { }}
+      setInfoAlert={() => { }} 
     />);
 
     const cityTextBox = CitySearchComponent.queryByRole('textbox'); 
@@ -84,6 +91,7 @@ describe('<CitySearch /> component', () => {
     CitySearchComponent.rerender(<CitySearch
       allLocations={allLocations}
       setCurrentCity={() => { }}
+      setInfoAlert={() => { }}
     />);
 
     const cityTextBox = CitySearchComponent.queryByRole('textbox'); 
