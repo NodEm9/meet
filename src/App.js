196115282b485dd14a6d2d1b9957f7c150d73155
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-08-06 10:24:14 +0200

    Render error and info alert components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navigation from './components/Navigation';
 import NumberOfEvents from './components/NumberOfEvents';
 import { extractLocations, getEvents } from './api';
 import loca from './images/loca.jpg';
+import { InfoAlert, ErrorAlert } from './components/Alert';
 
 
 
@@ -15,6 +16,8 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -23,20 +26,31 @@ const App = () => {
   const fetchData = async () => {
     const allEvents = await getEvents();
     const filteredEvents = currentCity === "See all cities" ?
-      allEvents : allEvents.filter(event => event?.location === currentCity)
+      allEvents : allEvents.filter(event => event.location === currentCity)
     const currentEvents = await filteredEvents.slice(0, currentNOE)
     setEvents(currentEvents);
     setAllLocations(extractLocations(allEvents));
   }
-
-
-  return (
+ 
+ 
+  return ( 
     <div className="App">
       <h1>Meet App</h1>
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+      </div>
       <section className="search">
-        <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+        <CitySearch
+          allLocations={allLocations}
+          setCurrentCity={setCurrentCity}
+          setInfoAlert={setInfoAlert}
+        />
           <img src={loca} alt='location' />
-          <NumberOfEvents setCurrentNOE={setCurrentNOE} />
+        <NumberOfEvents
+          setCurrentNOE={setCurrentNOE}
+          setErrorAlert={setErrorAlert}
+        />
       </section>
       <EventList events={events} />
     </div>
