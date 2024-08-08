f19935ceaf0833670dba98899fe4cf950739a5ae
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-08-08 11:24:42 +0200

    Add a PieChart for more data visualization

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -2,12 +2,13 @@ import Event from './Event'
 
 const EventList = ({ events }) => {
   return (
-     <ul id="event-list">
+    <ul id="event-list">
+      <h3>Events</h3>
       {events ? events.map(event => (
         <Event key={event.id} event={event} />
       )) : []}
       </ul> 
-  )
+  ) 
 }
 
 export default EventList
